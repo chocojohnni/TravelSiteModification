734cc2766ec839c17aa5b57a365792a7112e1f85
Johnny Tan <tul38243@temple.edu>
2025-11-18 01:30:24 -0500

    Commit LoginViewModel

---
+++ TravelSiteModification/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelSiteModification.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        private string Email { get; set; }
+
+        [Required]
+        private string Password { get; set; }
+
+        private string Message { get; set; }
+    }
+}
