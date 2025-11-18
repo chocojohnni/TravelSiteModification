cd83d01bd5fd943ab91a40f50e497671693378af
HonzTM <tup84792@temple.edu>
2025-11-18 13:55:19 -0500

    Changed TravelSiteController

--- TravelSiteModification/Controllers/TravelSiteController.cs
+++ TravelSiteModification/Controllers/TravelSiteController.cs
@@ -25,9 +25,45 @@ namespace TravelSiteModification.Controllers
         }
 
         [HttpPost]
-        public IActionResult SearchFlights(string flightDestination)
+        public IActionResult SearchFlights(string origin, string destination)
         {
+            if (string.IsNullOrEmpty(origin) || string.IsNullOrEmpty(destination))
+            {
+                return RedirectToAction("Index");
+            }
 
+            HttpContext.Session.SetString("FlightOrigin", origin);
+            HttpContext.Session.SetString("FlightDestination", destination);
+
+            return RedirectToAction("Index", "Flights");
+        }
+
+        [HttpPost]
+        public IActionResult SearchCars(string pickupLocation, string dropoffLocation, string pickupDate, string dropoffDate)
+        {
+            HttpContext.Session.SetString("CarPickupLocation", pickupLocation);
+            HttpContext.Session.SetString("CarDropoffLocation", dropoffLocation);
+            HttpContext.Session.SetString("CarPickupDate", pickupDate);
+            HttpContext.Session.SetString("CarDropoffDate", dropoffDate);
+
+            return RedirectToAction("Index", "Cars");
+        }
+
+        [HttpPost]
+        public IActionResult SearchEvents(string eventLocation)
+        {
+            HttpContext.Session.SetString("EventLocation", eventLocation);
+            return RedirectToAction("Index", "Events");
+        }
+
+        [HttpPost]
+        public IActionResult SearchPackages(string origin, string destination, string date)
+        {
+            HttpContext.Session.SetString("PackageOrigin", origin);
+            HttpContext.Session.SetString("PackageDestination", destination);
+            HttpContext.Session.SetString("PackageDate", date);
+
+            return RedirectToAction("Index", "Packages");
         }
     }
 }
