fe9110f47ea001a4126752cadf95b18b56087b7a
Johnny Tan <tul38243@temple.edu>
2025-11-19 22:41:15 -0500

    Updated LoginViewModel by adding RegularExpressions

--- TravelSiteModification/Models/LoginViewModel.cs
+++ TravelSiteModification/Models/LoginViewModel.cs
@@ -4,11 +4,24 @@ namespace TravelSiteModification.Models
 {
     public class LoginViewModel
     {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        private string email;
+        private string password;
 
         [Required]
-        public string Password { get; set; }
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d).{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long, include one uppercase letter and one number.")]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
     }
 }
