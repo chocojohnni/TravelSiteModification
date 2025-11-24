1922e6d003e485544b6b7523975a638347d0380b
HonzTM <tup84792@temple.edu>
2025-11-24 12:26:09 -0500

    Integrated EventsWebAPI into the solution. The search function itself is still not working but I will look into that

---
+++ EventsWebAPI/Models/EventActivity.cs
@@ -0,0 +1,55 @@
+﻿namespace EventsWebAPI.Models
+{
+    public class EventActivity
+    {
+        private int eventId;
+        private string eventName;
+        private string city;
+        private DateTime eventDate;
+        private decimal ticketPrice;
+        private string description;
+        private string imagePath;
+
+        public int EventId
+        {
+            get { return eventId; }
+            set { eventId = value; }
+        }
+
+        public string EventName
+        {
+            get { return eventName; }
+            set { eventName = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public DateTime EventDate
+        {
+            get { return eventDate; }
+            set { eventDate = value; }
+        }
+
+        public decimal TicketPrice
+        {
+            get { return ticketPrice; }
+            set { ticketPrice = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+    }
+}
---
+++ TravelSiteModification/Services/EventsAPIClient.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TravelSiteModification.Models;
+
+namespace TravelSiteModification.Services
+{
+    public class EventsAPIClient
+    {
+        private readonly HttpClient client;
+
+        public EventsAPIClient(HttpClient httpClient)
+        {
+            client = httpClient;
+        }
+
+        public async Task<List<EventActivity>> GetActivitiesAsync(string city, string state)
+        {
+            string url =
+                "api/Event/Activities?city=" +
+                Uri.EscapeDataString(city) +
+                "&state=" +
+                Uri.EscapeDataString(state);
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new List<EventActivity>();
+            }
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions();
+            options.PropertyNameCaseInsensitive = true;
+
+            List<EventActivity> events =
+                JsonSerializer.Deserialize<List<EventActivity>>(json, options);
+
+            if (events == null)
+            {
+                events = new List<EventActivity>();
+            }
+
+            return events;
+        }
+    }
+}
