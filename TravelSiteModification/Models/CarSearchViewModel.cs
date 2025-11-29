dc90a40f60c2bfbea8a8f9df122ec91ac260e9b7
johnnyboii <johnnyboii@JOHNNYTANDE70>
2025-11-29 03:06:54 -0500

    Added car stuff

--- TravelSiteModification/Models/CarSearchViewModel.cs
+++ TravelSiteModification/Models/CarSearchViewModel.cs
@@ -6,9 +6,11 @@
         public string DropoffLocation { get; set; }
         public DateTime PickupDate { get; set; }
         public DateTime DropoffDate { get; set; }
+        public DateTime PickupDateString { get; set; }
+        public DateTime DropoffDateString { get; set; }
 
         public List<string> Locations { get; set; } = new List<string>();
-        public List<CarResultViewModel> Results { get; set; } = new List<CarResultViewModel>();
+        public List<CarResultsViewModel> Results { get; set; } = new List<CarResultsViewModel>();
         public string ErrorMessage { get; set; }
 
         public string Message { get; set; }
