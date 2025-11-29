d68bb110f566d9bbb6c7407dea433bcf97732564
johnnyboii <johnnyboii@JOHNNYTANDE70>
2025-11-29 06:13:27 -0500

    Everything fixed

--- TravelSiteModification/Models/CarResultsViewModel.cs
+++ TravelSiteModification/Models/CarResultsViewModel.cs
@@ -1,19 +1,24 @@
-﻿namespace TravelSiteModification.Models
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TravelSiteModification.Models
 {
     public class CarResultsViewModel
     {
         public string PickupLocation { get; set; }
         public string DropoffLocation { get; set; }
-
         public DateTime PickupDate { get; set; }
         public DateTime DropoffDate { get; set; }
 
+
         public List<CarResultItem> Cars { get; set; } = new();
+
+
+        public bool IsDetailView { get; set; }
         public CarDetailViewModel CarDetails { get; set; }
-
-        public bool IsDetailView { get; set; } = false;
-
-        public List<string> Locations { get; set; } = new() { "NYC", "LAX", "MIA", "SEA" };
+        public List<CarResultItem> Results { get; set; } = new List<CarResultItem>();
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
     }
-
 }
