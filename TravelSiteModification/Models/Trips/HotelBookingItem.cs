18d162e638a140d45f03ad04151d38a1430a8bcb
Johnny Tan <tul38243@temple.edu>
2025-12-02 01:11:06 -0500

    Added TripsController, some trip view models, and one index for Trips. Also fixed logo action to return to TravelSite index so that user doesn't bypass login.

---
+++ TravelSiteModification/Models/Trips/HotelBookingItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelSiteModification.Models.Trips
+{
+    public class HotelBookingItem
+    {
+        public string HotelName { get; set; }
+        public string RoomType { get; set; }
+        public string Status { get; set; }
+        public string TotalPrice { get; set; }
+
+        public string CheckInDate { get; set; }
+        public string CheckOutDate { get; set; }
+    }
+}
