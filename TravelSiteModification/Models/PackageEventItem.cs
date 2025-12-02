3b250634a2002258415ded1feaeae9abed2bbaaf
HonzTM <129218090+HonzTM@users.noreply.github.com>
2025-12-02 13:56:05 -0500

    Added package files

---
+++ TravelSiteModification/Models/PackageEventItem.cs
@@ -0,0 +1,48 @@
+﻿namespace TravelSiteModification.Models
+{
+    public class PackageEventItem
+    {
+        private int packageEventId;
+        private int eventId;
+        private string eventName;
+        private string eventLocation;
+        private DateTime eventDate;
+        private decimal price;
+
+        public int PackageEventId
+        {
+            get { return packageEventId; }
+            set { packageEventId = value; }
+        }
+
+        public int EventId
+        {
+            get { return eventId; }
+            set { eventId = value; }
+        }
+
+        public string EventName
+        {
+            get { return eventName; }
+            set { eventName = value; }
+        }
+
+        public string EventLocation
+        {
+            get { return eventLocation; }
+            set { eventLocation = value; }
+        }
+
+        public DateTime EventDate
+        {
+            get { return eventDate; }
+            set { eventDate = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
---
+++ TravelSiteModification/Models/PackageHotelItem.cs
@@ -0,0 +1,69 @@
+﻿namespace TravelSiteModification.Models
+{
+    public class PackageHotelItem
+    {
+        private int packageHotelId;
+        private int hotelId;
+        private int roomId;
+        private string hotelName;
+        private string city;
+        private string state;
+        private DateTime checkInDate;
+        private DateTime checkOutDate;
+        private decimal totalPrice;
+
+        public int PackageHotelId
+        {
+            get { return packageHotelId; }
+            set { packageHotelId = value; }
+        }
+
+        public int HotelId
+        {
+            get { return hotelId; }
+            set { hotelId = value; }
+        }
+
+        public int RoomId
+        {
+            get { return roomId; }
+            set { roomId = value; }
+        }
+
+        public string HotelName
+        {
+            get { return hotelName; }
+            set { hotelName = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public DateTime CheckInDate
+        {
+            get { return checkInDate; }
+            set { checkInDate = value; }
+        }
+
+        public DateTime CheckOutDate
+        {
+            get { return checkOutDate; }
+            set { checkOutDate = value; }
+        }
+
+        public decimal TotalPrice
+        {
+            get { return totalPrice; }
+            set { totalPrice = value; }
+        }
+    }
+}
